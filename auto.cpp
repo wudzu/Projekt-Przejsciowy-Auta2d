b59de7c1ea53b282ca32d1c0f789aafde1896488
AndyMaster <andy356@wp.pl>
2015-03-23 20:38:39 +0100

    Zalążki modelu pojazdu cd.

--- auto.cpp
+++ auto.cpp
@@ -1,1 +1,24 @@
 #include "auto.h"
+
+void Engine::testParam(float Maxpred, float Przys, float Hamulce, float Ster)
+{
+    float suma;
+    suma=Maxpred+Przys+Hamulce+Ster;
+
+    if(suma>1.0)
+        Error(0);
+    else;
+}
+
+void Engine::Error(int idbledu)
+{
+    switch(idbledu){
+    case 0:
+        {
+            printf("Z³y stosunek parametrów pojazdu.\n");
+            blad=true;
+            break;
+        }
+    }
+
+}
--- auto.h
+++ auto.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <stdio.h>
 
 class Auto;
 class Engine;
@@ -12,11 +13,13 @@ class Engine
 {
     Engine();
     ~Engine();
+    bool blad; //Gdy jest program przestaje dzia³aæ.
     std::vector <Auto> gracze; //Wektor zawieraj¹cy wszystkich graczy.
     void testParam(float Maxpred, float Przys, float Hamulce, float Ster); //Sprawdza czy gracz nie wyda³ za du¿o punktów.
     void ustawPredkosc(float Mapred, float Przys, float Hamulce); //Ustala aktualn¹ prêdkoœæ pojazdu.
     void ustawSkret(float Ster, float katKol, float katAuta); //Porownuje pozycjê kó³ wzglêdem auta i koryguje k¹t skrêtu.
     void Nawierzchnia(); //Sprawdza po czym jedzie auto i okreœla wp³yw na jazdê.
+    void Error(int idbledu); //Informuje usera o b³êdach.
 };
 
 class Auto
