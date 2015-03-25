9d086518f34ecbd0c2de9a21cfee480ee4145ef1
AndyMaster <andy356@wp.pl>
2015-03-25 09:42:20 +0100

    Kontrola predkosci

--- auto.cpp
+++ auto.cpp
@@ -1,22 +1,48 @@
 #include "auto.h"
 
+
+
 void Engine::testParam(float statMaxpr, float statPrzys, float statSter)
 {
     float suma;
     suma=statMaxpr+statPrzys+statSter;
 
-    if(suma>1.0)
+    if(suma>10)
         Error(0);
-    else;
+    else
+    {
+        gracze[0].maxZmiana=10+10*sin(statPrzys);
+        gracze[0].maxSkret=10+10*sin(statSter);
+        gracze[0].maxPred=220+220*sin(statMaxpr);
+    }
+
 }
 
-void Engine::ustawPredkosc(float Maxpred, sf::Vector2f Przys)
+void Engine::ustawPredkosc(float maxPred, sf::Vector2f Przys)
 {
-   // Auto::predkosc.x=
-    if(Auto.predkosc.x>Maxpred)
-        Auto.predkosc.x=Maxpred;
-    if(Auto.pozycja.y>Maxpred)
-        Auto.pozycja.y>Maxpred;
+    if(Przys.x<=gracze[0].maxZmiana && Przys.x>0)
+        gracze[0].predkosc.x+=Przys.x;
+    else if(Przys.x>gracze[0].maxZmiana)
+        gracze[0].predkosc.x+=gracze[0].maxZmiana;
+    else
+        gracze[0].predkosc.x-=gracze[0].maxZmiana; //hamowanie dla przys=0
+
+    if(Przys.y<=gracze[0].maxZmiana && Przys.y>0)
+        gracze[0].predkosc.y+=Przys.y;
+    else if(Przys.y>gracze[0].maxZmiana)
+        gracze[0].predkosc.y+=gracze[0].maxZmiana;
+    else
+        gracze[0].predkosc.y-=gracze[0].maxZmiana; //hamowanie dla przys=0
+
+    if(gracze[0].predkosc.x>maxPred)
+        gracze[0].predkosc.x=maxPred;
+    if(gracze[0].pozycja.y>maxPred)
+        gracze[0].pozycja.y=maxPred;
+}
+
+void ustawSkret(sf::Vector2f Ster, sf::Vector2f katKol, sf::Vector2f katAuta)
+{
+
 }
 
 void Engine::Error(int idbledu)
--- auto.h
+++ auto.h
@@ -17,8 +17,8 @@ public:
     ~Engine();
     bool blad; //Gdy jest program przestaje dzia³aæ.
     std::vector <Auto> gracze; //Wektor zawieraj¹cy wszystkich graczy.
-    void testParam(float statMaxpr, float statPrzys, float statSter); //Sprawdza czy gracz nie wyda³ za du¿o punktów.
-    void ustawPredkosc(float Maxpred, sf::Vector2f Przys); //Ustala aktualn¹ prêdkoœæ pojazdu.
+    void testParam(float statMaxpr, float statPrzys, float statSter); //Sprawdza czy gracz nie wyda³ za du¿o punktów i oblicz maksymalne wartosci.
+    void ustawPredkosc(float maxPred, sf::Vector2f Przys); //Ustala aktualn¹ prêdkoœæ pojazdu.
     void ustawSkret(sf::Vector2f Ster, sf::Vector2f katKol, sf::Vector2f katAuta); //Porownuje pozycjê kó³ wzglêdem auta i koryguje k¹t skrêtu.
     void Nawierzchnia(); //Sprawdza po czym jedzie auto i okreœla wp³yw na jazdê.
     void Error(int idbledu); //Informuje usera o b³êdach.
@@ -39,9 +39,11 @@ public:
     sf::Vector2f kopiaSter;
     float statMaxpr, statPrzys, statSter; //Zmienne wplywajace na wlasciwosci pojazdu.
     //float statHam;
-    float Maxpred; //Maksymalna prêdkoœæ pojazdu.
+    float maxPred; //Maksymalna prêdkoœæ pojazdu.
     sf::Vector2f Przys; //Przyspieszenie auta.
     //sf::Vector2f Hamulce; //Moc hamulców.
     sf::Vector2f Ster; //Sterownoœæ.
     sf::Vector2f katAuta; //Pozycja samochodu wzglêdem kó³.
+    float maxZmiana; // Maksymalne wartosci obliczane z rozdanych punktow.
+    float maxSkret;
 };
