4d2f3d7ca913635a51f8d41f8e365f496f3aacc2
wudzu <wudzu1@gmail.com>
2015-03-28 16:52:32 +0100

    Poprawki

--- auto.cpp
+++ auto.cpp
@@ -1,14 +1,16 @@
 #include "auto.h"
 
-void Engine::Init()
+Engine::Engine()
 {
-    gracze[0].tempPrzyspieszenie.x=0;
-    gracze[0].tempPrzyspieszenie.y=0;
+    Auto a;
+    gracze.push_back(a);
+    gracze[0].przyspieszenie.x=0;
+    gracze[0].przyspieszenie.y=0;
     klatka=0.5;
-    gracze[0].tempKat.x=0;
-    gracze[0].tempKat.y=1;
-    gracze[0].tempPredkosc.x=0;
-    gracze[0].tempPredkosc.y=0;
+    gracze[0].orientacja.x=0;
+    gracze[0].orientacja.y=1;
+    gracze[0].V.x=0;
+    gracze[0].V.y=0;
 }
 
 void Engine::testParam(float statMaxpr, float statPrzys, float statSter)
@@ -27,45 +29,46 @@ void Engine::testParam(float statMaxpr, float statPrzys, float statSter)
 
 }
 
-void Engine::ustawTrajektorie(float statMaxpr, float statSter, float statPrzys, sf::Vector2f zmiana)
+void Engine::ustawTrajektorie(int nrAuta)
 {
-    gracze[0].tempPredkosc=gracze[0].tempPredkosc+gracze[0].tempPrzyspieszenie*klatka;
+    gracze[nrAuta].V=gracze[nrAuta].V+gracze[nrAuta].przyspieszenie*klatka;
 
-    if(gracze[0].tempPredkosc.x>statMaxpr)
-        gracze[0].tempPredkosc.x=statMaxpr;
-    else if(gracze[0].tempPredkosc.x<-statMaxpr)
-        gracze[0].tempPredkosc.x=-statMaxpr;
+/*
+    if(gracze[nrAuta].tempPredkosc.x>statMaxpr)
+        gracze[nrAuta].tempPredkosc.x=statMaxpr;
+    else if(gracze[nrAuta].tempPredkosc.x<-statMaxpr)
+        gracze[nrAuta].tempPredkosc.x=-statMaxpr;
 
     if(gracze[0].tempPredkosc.y>statMaxpr)
         gracze[0].tempPredkosc.y=statMaxpr;
     else if(gracze[0].tempPredkosc.y<-statMaxpr)
         gracze[0].tempPredkosc.y=-statMaxpr;
+*/
+    gracze[nrAuta].pozycja= gracze[nrAuta].pozycja+gracze[nrAuta].V*klatka;
 
-    gracze[0].tempPozycja= gracze[0].tempPozycja+gracze[0].tempPredkosc*klatka;
+    temp1=gracze[nrAuta].orientacja*(gracze[nrAuta].orientacja.x*gracze[nrAuta].V.x+gracze[nrAuta].orientacja.y*gracze[nrAuta].V.y);
+    temp1=gracze[nrAuta].V-temp1;
 
-    temp1=gracze[0].tempKat*(gracze[0].tempKat.x*gracze[0].tempPredkosc.x+gracze[0].tempKat.y*gracze[0].tempPredkosc.y);
-    temp1=gracze[0].tempPredkosc-temp1;
+    gracze[nrAuta].przyspieszenie=temp1*(-3.0f)*sprawdzTarcie(nrAuta)-gracze[nrAuta].V*sprawdzTarcie(nrAuta);
 
-    gracze[0].tempPrzyspieszenie=temp1*(float)(-3.0)*sprawdzTarcie()-gracze[0].tempPredkosc*sprawdzTarcie();
+    gracze[nrAuta].orientacja.x=gracze[nrAuta].orientacja.x*cos(gracze[nrAuta].sterowanie.y*klatka*PI/45*klatka*gracze[nrAuta].statSter)-gracze[nrAuta].orientacja.y*sin(gracze[nrAuta].sterowanie.y*klatka*PI/45*klatka*gracze[nrAuta].statSter);
+    gracze[nrAuta].orientacja.y=gracze[nrAuta].orientacja.y*cos(gracze[nrAuta].sterowanie.y*klatka*PI/45*klatka*gracze[nrAuta].statSter)+gracze[nrAuta].orientacja.x*sin(gracze[nrAuta].sterowanie.y*klatka*PI/45*klatka*gracze[nrAuta].statSter);
 
-    gracze[0].tempKat.x=gracze[0].tempKat.x*cos(gracze[0].zmiana.y*klatka*PI/180*4)-gracze[0].tempKat.y*sin(gracze[0].zmiana.y*klatka*PI/180*4);
-    gracze[0].tempKat.y=gracze[0].tempKat.y*cos(gracze[0].zmiana.y*klatka*PI/180*4)+gracze[0].tempKat.x*sin(gracze[0].zmiana.y*klatka*PI/180*4);
+    temp2=sqrt(gracze[nrAuta].orientacja.x*gracze[nrAuta].orientacja.x+gracze[nrAuta].orientacja.y*gracze[nrAuta].orientacja.y);
 
-    temp2=(float)sqrt(gracze[0].tempKat.x*gracze[0].tempKat.x+gracze[0].tempKat.y*gracze[0].tempKat.y);
+    gracze[nrAuta].orientacja=gracze[nrAuta].orientacja/temp2;
 
-    gracze[0].tempKat=gracze[0].statSter*gracze[0].tempKat/temp2;
+    gracze[nrAuta].przyspieszenie=gracze[nrAuta].przyspieszenie+gracze[nrAuta].sterowanie.x*gracze[nrAuta].orientacja;
+    gracze[nrAuta].przyspieszenie=gracze[nrAuta].przyspieszenie*sprawdzTarcie(nrAuta);
 
-    gracze[0].tempPrzyspieszenie=gracze[0].tempPrzyspieszenie+gracze[0].zmiana.x*gracze[0].tempKat;
-    gracze[0].tempPrzyspieszenie=gracze[0].statPrzys*gracze[0].tempPrzyspieszenie*sprawdzTarcie();
-
-    gracze[0].stanWyjscia[0]=gracze[0].tempPozycja;
-    gracze[0].stanWyjscia[1]=gracze[0].tempKat;
-    gracze[0].stanWyjscia[2]=gracze[0].tempPrzyspieszenie;
+    //gracze[0].stanWyjscia[0]=gracze[0].tempPozycja;
+    //gracze[0].stanWyjscia[1]=gracze[0].orientacja;
+    //gracze[0].stanWyjscia[2]=gracze[0].tempPrzyspieszenie;
 }
 
-float Engine::sprawdzTarcie()
+float Engine::sprawdzTarcie(int nrAuta)
 {
-    return 1;
+    return tarcie[nrAuta];
 }
 
 void Engine::Error(int idbledu)
@@ -78,5 +81,20 @@ void Engine::Error(int idbledu)
             break;
         }
     }
+
+}
+
+void Engine::nawierzchnia()
+{
+    //ustawia odpowiednie wartosci na wektorze z tarciem
+}
+
+Auto::Auto()
+{
+
+}
+
+Auto::~Auto()
+{
 
 }
--- auto.h
+++ auto.h
@@ -16,38 +16,47 @@ class Engine
 public:
     Engine();
     ~Engine();
+
+private:
     float klatka; //dlugosc trwania klatki
     sf::Vector2f temp1;
     float temp2;
     bool blad; //Gdy jest program przestaje dziala.
     std::vector <Auto> gracze; //Wektor zawieraj¹cy wszystkich graczy.
     void testParam(float statMaxpr, float statPrzys, float statSter); //Sprawdza czy gracz nie wydal za du¿o punktów i oblicz maksymalne wartosci.
-    void ustawTrajektorie(float statMaxpr, float statSter, float statPrzys, sf::Vector2f zmiana); //Ustala aktualna predkosc pojazdu.
-    void Nawierzchnia(); //Sprawdza po czym jedzie auto i okresla wplyw na jazde.
+    void ustawTrajektorie(int nrAuta); //Ustala aktualna predkosc pojazdu.
+    void nawierzchnia(); //Sprawdza po czym jedzie auto i okresla wplyw na jazde.
     void Error(int idbledu); //Informuje usera o bledach.
-    void Init(); //Funkcja inicjalizujaca wartosci poczatkowe.
-    float sprawdzTarcie();
+    //void Init(); //Funkcja inicjalizujaca wartosci poczatkowe.
+    float sprawdzTarcie(int nrAuta);
+    std::vector <int> tarcie;
 };
 
 class Auto
 {
+    friend class Engine;
 public:
     Auto();
     ~Auto();
     int id;
-    sf::Vector2f zmiana; //Modyfikowana predkosc i skret. x-predkosc y-skret
-    sf::Vector2f tempPozycja; //Pozycja auta na trasie.
-    sf::Vector2f tempKat;
-    sf::Vector2f tempPredkosc;
-    sf::Vector2f tempPrzyspieszenie;
-    sf::Vector2f stanWyjscia[3];
+    //sf::Vector2f tempPozycja; //Pozycja auta na trasie.
+    //sf::Vector2f tempKat;
+    //sf::Vector2f tempPredkosc;
+    //sf::Vector2f tempPrzyspieszenie;
+    //sf::Vector2f stanWyjscia[3];
     float statMaxpr, statPrzys, statSter; //Zmienne wplywajace na wlasciwosci pojazdu.
 
+    private:
     //zmienne JESZCZE nie uzyte
-    float kopiaMaxpred; //Zmienne ustalane przez gracza.
-    sf::Vector2f kopiaPrzys;
-    sf::Vector2f kopiaSter;
-    sf::Vector2f Przys; //Przyspieszenie auta.
-    sf::Vector2f Ster; //Sterownosc.
-    sf::Vector2f katAuta; //Pozycja samochodu wzgledem kol.
+    //float kopiaMaxpred; //Zmienne ustalane przez gracza.
+    sf::Vector2f kopiaPrzyspieszenie;
+    sf::Vector2f kopiaSterowanie;
+    sf::Vector2f kopiaOrientacja;
+    sf::Vector2f V;
+    sf::Vector2f kopiaV;
+    sf::Vector2f przyspieszenie; //Przyspieszenie auta.
+    sf::Vector2f sterowanie; //
+    sf::Vector2f orientacja; //Pozycja samochodu wzgledem kol.
+    sf::Vector2f pozycja;
+    sf::Vector2f kopiaPozycja;
 };
