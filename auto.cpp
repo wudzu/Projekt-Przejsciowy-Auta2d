a842d4f0633130f08249b6383a2d70fb917317eb
AndyMaster <andy356@wp.pl>
2015-04-11 13:00:39 +0200

    One name to rule all the functions

--- auto.cpp
+++ auto.cpp
@@ -211,17 +211,16 @@ void Engine::petlaGlowna()
     {
     case 4:
         DLL3= LoadLibrary("4/DLL.dll");
-        ster3=(POBRANE) GetProcAddress(DLL3, "sterowanie1");
-
+        sterowanie=(POBRANE) GetProcAddress(DLL3, "sterowanie");
     case 3:
         DLL2= LoadLibrary("3/DLL.dll");
-        ster2=(POBRANE) GetProcAddress(DLL2, "sterowanie1");
+        sterowanie=(POBRANE) GetProcAddress(DLL2, "sterowanie");
     case 2:
         DLL1= LoadLibrary("2/DLL.dll");
-        ster1=(POBRANE) GetProcAddress(DLL1, "sterowanie1");
+        sterowanie=(POBRANE) GetProcAddress(DLL1, "sterowanie");
     case 1:
         DLL0= LoadLibrary("1/DLL.dll");
-        ster0=(POBRANE) GetProcAddress(DLL0, "brutal");
+        sterowanie=(POBRANE) GetProcAddress(DLL0, "sterowanie");
         break;
     }
     std::vector<std::thread> watki;
@@ -229,13 +228,13 @@ void Engine::petlaGlowna()
     switch (gracze.size())
     {
     case 4:
-     //   watki.push_back(std::thread(ster3, &(gracze[3].kopiaV.x),&(gracze[3].kopiaV.y),&(gracze[3].kopiaPrzyspieszenie.x),&(gracze[3].kopiaPrzyspieszenie.y),&(gracze[3].kopiaOrientacja.x),&(gracze[3].kopiaOrientacja.y),&(gracze[3].kopiaPozycja.x),&(gracze[3].kopiaPozycja.y),&(gracze[3].kopiaSterowanie.x),&(gracze[3].kopiaSterowanie.y),&dziala,mapaRGB));
+        watki.push_back(std::thread(sterowanie, &(gracze[3].kopiaV.x),&(gracze[3].kopiaV.y),&(gracze[3].kopiaPrzyspieszenie.x),&(gracze[3].kopiaPrzyspieszenie.y),&(gracze[3].kopiaOrientacja.x),&(gracze[3].kopiaOrientacja.y),&(gracze[3].kopiaPozycja.x),&(gracze[3].kopiaPozycja.y),&(gracze[3].kopiaSterowanie.x),&(gracze[3].kopiaSterowanie.y),&dziala,mapaRGB, &(meta.x),&(meta.y)));
     case 3:
-    //    watki.push_back(std::thread(ster2, &(gracze[2].kopiaV.x),&(gracze[2].kopiaV.y),&(gracze[2].kopiaPrzyspieszenie.x),&(gracze[2].kopiaPrzyspieszenie.y),&(gracze[2].kopiaOrientacja.x),&(gracze[2].kopiaOrientacja.y),&(gracze[2].kopiaPozycja.x),&(gracze[2].kopiaPozycja.y),&(gracze[2].kopiaSterowanie.x),&(gracze[2].kopiaSterowanie.y),&dziala,mapaRGB));
+        watki.push_back(std::thread(sterowanie, &(gracze[2].kopiaV.x),&(gracze[2].kopiaV.y),&(gracze[2].kopiaPrzyspieszenie.x),&(gracze[2].kopiaPrzyspieszenie.y),&(gracze[2].kopiaOrientacja.x),&(gracze[2].kopiaOrientacja.y),&(gracze[2].kopiaPozycja.x),&(gracze[2].kopiaPozycja.y),&(gracze[2].kopiaSterowanie.x),&(gracze[2].kopiaSterowanie.y),&dziala,mapaRGB, &(meta.x),&(meta.y)));
     case 2:
-     //   watki.push_back(std::thread(ster1, &(gracze[1].kopiaV.x),&(gracze[1].kopiaV.y),&(gracze[1].kopiaPrzyspieszenie.x),&(gracze[1].kopiaPrzyspieszenie.y),&(gracze[1].kopiaOrientacja.x),&(gracze[1].kopiaOrientacja.y),&(gracze[1].kopiaPozycja.x),&(gracze[1].kopiaPozycja.y),&(gracze[1].kopiaSterowanie.x),&(gracze[1].kopiaSterowanie.y),&dziala,mapaRGB));
+        watki.push_back(std::thread(sterowanie, &(gracze[1].kopiaV.x),&(gracze[1].kopiaV.y),&(gracze[1].kopiaPrzyspieszenie.x),&(gracze[1].kopiaPrzyspieszenie.y),&(gracze[1].kopiaOrientacja.x),&(gracze[1].kopiaOrientacja.y),&(gracze[1].kopiaPozycja.x),&(gracze[1].kopiaPozycja.y),&(gracze[1].kopiaSterowanie.x),&(gracze[1].kopiaSterowanie.y),&dziala,mapaRGB, &(meta.x),&(meta.y)));
     case 1:
-        watki.push_back(std::thread(ster0, &(gracze[0].kopiaV.x),&(gracze[0].kopiaV.y),&(gracze[0].kopiaPrzyspieszenie.x),&(gracze[0].kopiaPrzyspieszenie.y),&(gracze[0].kopiaOrientacja.x),&(gracze[0].kopiaOrientacja.y),&(gracze[0].kopiaPozycja.x),&(gracze[0].kopiaPozycja.y),&(gracze[0].kopiaSterowanie.x),&(gracze[0].kopiaSterowanie.y),&dziala,mapaRGB, &(meta.x),&(meta.y)));
+        watki.push_back(std::thread(sterowanie, &(gracze[0].kopiaV.x),&(gracze[0].kopiaV.y),&(gracze[0].kopiaPrzyspieszenie.x),&(gracze[0].kopiaPrzyspieszenie.y),&(gracze[0].kopiaOrientacja.x),&(gracze[0].kopiaOrientacja.y),&(gracze[0].kopiaPozycja.x),&(gracze[0].kopiaPozycja.y),&(gracze[0].kopiaSterowanie.x),&(gracze[0].kopiaSterowanie.y),&dziala,mapaRGB, &(meta.x),&(meta.y)));
     }
 
 
--- auto.h
+++ auto.h
@@ -28,7 +28,7 @@ private:
     HINSTANCE DLL0,DLL1,DLL2,DLL3;
     typedef void( * POBRANE )(float*, float*, float*, float*, float*, float*, float*, float*, float*, float*, bool*, std::vector<std::vector<std::vector<unsigned char>>>,float*, float*);
     void tworzMape();
-    POBRANE ster0,ster1,ster2,ster3;
+    POBRANE sterowanie;
     sf::Vector2f meta;
     void znajdzMete();
     int sprawdzWygrana();
