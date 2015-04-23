88fe61b1499df17b9464e194e306cd040e3df623
wudzu <wudzu1@gmail.com>
2015-04-23 14:10:28 +0200

    Revert "Moje"
    This reverts commit 59bccf90e4157c3b8ef485d23b007483ccc0bd2f.

--- auto.cpp
+++ auto.cpp
@@ -253,7 +253,6 @@ void Engine::petlaGlowna()
 
 
     int wygrana=-1;
-    zegar.restart();
     while(dziala)
     {
         //test2(gracze[0].kopiaSterowanie.x, gracze[0].kopiaSterowanie.y);
@@ -368,14 +367,6 @@ void Engine::ustawTrajektorie(int nrAuta)
     gracze[nrAuta].kopiaOrientacja=gracze[nrAuta].orientacja;
     gracze[nrAuta].kopiaPozycja=gracze[nrAuta].pozycja;
     gracze[nrAuta].sterowanie=gracze[nrAuta].kopiaSterowanie;
-    if (gracze[nrAuta].sterowanie.x > 1.0f)
-        gracze[nrAuta].sterowanie.x=1.0f;
-    if (gracze[nrAuta].sterowanie.x < -1.0f)
-        gracze[nrAuta].sterowanie.x=-1.0f;
-    if (gracze[nrAuta].sterowanie.y > 1.0f)
-        gracze[nrAuta].sterowanie.y=1.0f;
-    if (gracze[nrAuta].sterowanie.y < -1.0f)
-        gracze[nrAuta].sterowanie.y=-1.0f;
 /*
     system("cls");
     printf("\n\n\n");
